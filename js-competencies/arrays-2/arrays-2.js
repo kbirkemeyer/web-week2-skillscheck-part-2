4655c753e334a9140371d77ff1d15107ce3c9fb5
Katie <adlerk8@gmail.com>
2020-10-16 14:37:15 -0700

    finished with arrays-2

--- js-competencies/arrays-2/arrays-2.js
+++ js-competencies/arrays-2/arrays-2.js
@@ -10,7 +10,12 @@
 //addMoreDucks should return the array.
 
 //Code here
-
+function addMoreDucks(arr) {
+    if(arr.length > 4){
+        arr[2] = "42 ducks";
+    }
+    return arr;
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -19,7 +24,12 @@
 //the 3rd element of the array. cutItOut should return the array.
 
 //Code here
-
+function cutItOut(arr) {
+    if(arr.length > 4){
+        arr.splice(2, 1);
+    }
+    return arr;
+}
 
 //////////////////PROBLEM 3////////////////////
 
@@ -28,4 +38,11 @@
 //changeName should return the array.
 
 //Code here
-
+function changeName(arr) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === "Tolkn"){
+            arr[i] = "Tolkien";
+        }
+    }
+    return arr;
+}
