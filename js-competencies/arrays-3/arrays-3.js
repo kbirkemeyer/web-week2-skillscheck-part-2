15b5a823d1d4f2f251c61b5a57cb8511aa016498
Katie <adlerk8@gmail.com>
2020-10-18 13:59:02 -0700

    in progress callbacks-1

--- js-competencies/arrays-3/arrays-3.js
+++ js-competencies/arrays-3/arrays-3.js
@@ -18,12 +18,6 @@ sunsetColors[0] = "blue";
 return newSunset;
 }
 
-// function sunset() {
-//     let newSunset = [...sunsetColors];
-//     sunsetCopy[0] = "blue";
-//     return sunsetCopy;
-// }
-
 //////////////////PROBLEM 2////////////////////
 
 //Create a function called sunrise that copies the value of the existing array
@@ -33,7 +27,11 @@ return newSunset;
 var sunriseColors = ["yellow", "orange", "red", "pink", "purple", "blue"]
 
 //Code here
-
+function sunrise() {
+    let newSunrise = sunriseColors.slice(0);
+    newSunrise[0] = "green";
+    return newSunrise;
+}
 
 //////////////////PROBLEM 3////////////////////
 
@@ -52,11 +50,11 @@ var sunriseColors = ["yellow", "orange", "red", "pink", "purple", "blue"]
 //                 the copy and the original; a copy made by value will only 
 //                 change the copy's values`
 
-// var refVsVal = `Passing by reference points to the same object in memory;
-//                 pass by value creates a new object in memory with the same 
-//                 values. A change made to a reference copy will change both 
-//                 the copy and the original; a copy made by value will only 
-//                 change the copy's values`
+var refVsVal = `Passing by reference points to the same object in memory;
+                pass by value creates a new object in memory with the same 
+                values. A change made to a reference copy will change both 
+                the copy and the original; a copy made by value will only 
+                change the copy's values`
 
 // var refVsVal = `Passing by reference creates a new array in memory that 
 //                 points to the original array in memory; pass by value creates 
