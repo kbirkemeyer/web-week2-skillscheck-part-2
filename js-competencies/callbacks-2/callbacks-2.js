e8eddeffdfd41567845d0d546d3d8800ee01bfae
Katie <adlerk8@gmail.com>
2020-10-18 17:50:04 -0700

    in progress callbacks-2

--- js-competencies/callbacks-2/callbacks-2.js
+++ js-competencies/callbacks-2/callbacks-2.js
@@ -29,7 +29,6 @@ function invokesCallbacks(num, cb1, cb2) {
   var greeting;
 
   function timedGreeting(callback){
-  greeting = setTimeout(callback, 
-    1000);
+  greeting = setTimeout(callback, 1000);
   }
 
