dc5697230bce8828ac76c47465d6279ed98c6acb
Katie <adlerk8@gmail.com>
2020-10-18 17:03:50 -0700

    in progress callbacks-1

--- js-competencies/callbacks-1/callbacks.js
+++ js-competencies/callbacks-1/callbacks.js
@@ -3,7 +3,16 @@
 // Write a function called stringItOut that takes in an array. Determine if it is an array of numbers or strings and sort it greatest to least accordingly (sort strings alphabetically). Return the array converted into a string using the toString method.
 
 //Code here
-
+function stringItOut(arr) {
+  arr.forEach(e => {
+    if(isNaN(e)){
+      arr.sort();
+    } else{
+      arr.sort((e, b) => b - e);
+    }
+  })
+  return arr.toString();
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -18,3 +27,4 @@ function complex(num1, num2) {
 }
 
 //Code here
+let mathResult = math(complex, 2, 3)
