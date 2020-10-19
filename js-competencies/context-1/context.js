663e1ae5d44411752b8769d2663cd27d85d36b71
Katie <adlerk8@gmail.com>
2020-10-18 20:04:30 -0700

    in progress closures, started context

--- js-competencies/context-1/context.js
+++ js-competencies/context-1/context.js
@@ -4,7 +4,7 @@
 // What is context? Uncomment the best answer.
 
 // var context = "The file which is the execution context of the running code, represented by 'context' keyword."
-// var context = "The object which is the execution context of the running code, represented by 'this' keyword.";
+var context = "The object which is the execution context of the running code, represented by 'this' keyword.";
 // var context = "The function which is the execution context of the running code, represented by 'this' keyword."
 // var context = "The browser window, represented by 'context' keyword."
 // var context = "The html page where the script is running, represented by 'this' keyword."
@@ -23,7 +23,7 @@ function rubberDucks() {
 rubberDucks();
 
 // var contextRubberDucks = "index.html"
-// var contextRubberDucks = 'window';
+var contextRubberDucks = 'window';
 // var contextRubberDucks = "Object"
 // var contextRubberDucks = "rubberDucks"
 // var contextRubberDucks = "context.js"
@@ -46,7 +46,7 @@ var dcMetro = {
 // var contextBreakMetro = "index.html"
 // var contextBreakMetro = "window"
 // var contextBreakMetro = "Object"
-// var contextBreakMetro = 'dcMetro';
+var contextBreakMetro = 'dcMetro';
 // var contextBreakMetro = "context.js"
 
 
@@ -64,25 +64,25 @@ var ascent = {
 };
 
 // CODE HERE
-
+var areWeThereYet = ascent.remainingElevation;
 
 // Now, invoke areWeThereYet and save the result to
 // a variable named moreToGo.
 
 // CODE HERE
-
+var moreToGo = areWeThereYet;
 
 // Now, uncomment the best answers:
 
 // What will the value of moreToGo be?
 // var valMoreToGo = 1500
 // var valMoreToGo = NaN;
-// var valMoreToGo = undefined
+var valMoreToGo = undefined
 // var valMoreToGo = -1500
 
 // What was the context of areWeThereYet when you ran it?
 // var contextAreWeThereYet = "ascent"
-// var contextAreWeThereYet = 'window';
+var contextAreWeThereYet = 'window';
 // var contextAreWeThereYet = "Object"
 // var contextAreWeThereYet = "context.js"
 
@@ -102,13 +102,13 @@ var descent = {
 };
 
 // CODE HERE
-
+var almostThere = descent.remainingElevation;
 
 // Now, invoke almostThere and save the result to
 // a variable named finalDescent.
 
 // CODE HERE
-
+let finalDescent = almostThere;
 
 // Now, uncomment the best answers:
 
@@ -116,10 +116,10 @@ var descent = {
 // var valFinalDescent = 21500
 // var valFinalDescent = NaN
 // var valFinalDescent = undefined
-// var valFinalDescent = -21500;
+var valFinalDescent = -21500;
 
 // What was the context of almostThere when you ran it?
-// var contextAlmostThere = 'descent';
+var contextAlmostThere = 'descent';
 // var contextAlmostThere = "window"
 // var contextAlmostThere = "Object"
 // var contextAlmostThere = "context.js"
