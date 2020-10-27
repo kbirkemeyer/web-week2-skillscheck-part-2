316c0bd36e5d7c339a037145032a4d302f69bfb8
Katie <adlerk8@gmail.com>
2020-10-27 14:48:11 -0700

    in progress for loops

--- js-competencies/for-loops/for-loops.js
+++ js-competencies/for-loops/for-loops.js
@@ -11,7 +11,9 @@
   function addNumbers(value){
       var myNumbers = []
       //Code here
-      
+      for(let i = 0; i < value; i++)
+      myNumbers.push(i);
+      return myNumbers;
   }
 
 
@@ -25,7 +27,10 @@
   function backwardsGreeting(arr) {
     let greetings = [];
     //Code here
-  
+    for(let i = arr.length - 1; i >= 0 ; i--){
+      greetings.push(`Hello, ${arr[i]}`);
+    }
+    return greetings;
   }
 
 
