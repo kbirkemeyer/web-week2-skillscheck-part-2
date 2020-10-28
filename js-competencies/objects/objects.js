1e6627435562543838260ec96eb401dd5e35bd2a
Katie <adlerk8@gmail.com>
2020-10-27 19:45:33 -0700

    in progress objects, scope done

--- js-competencies/objects/objects.js
+++ js-competencies/objects/objects.js
@@ -12,13 +12,13 @@ var duck = {
 //1. Using dot notation to access the duck's "name" property.
 // var accessByDotNotation = duck."name"
 // var accessByDotNotation = Object[duck].name
-// var accessByDotNotation = duck.name
+var accessByDotNotation = duck.name
 
 //2. Using bracket notation to access duck's "bites" property.
 // var accessByBracketNotation = duck{"bites"}
 // var accessByBracketNotation = Object[duck][bites]
 // var accessByBracketNotation = duck[bites]
-// var accessByBracketNotation = duck["bites"]
+var accessByBracketNotation = duck["bites"]
 // var accessByBracketNotation = duck(bites)
 
 
@@ -35,7 +35,9 @@ var person = {
 }
 
 //CODE HERE
-
+person.moveCities = function (newCity) {
+  person.city = newCity;
+}
 
 ///////////////////Problem 3///////////////////
 
@@ -67,7 +69,11 @@ var sampleOutput = [
 ]
 
 //CODE HERE
+function keyAccessor(arr, obj) {
+  for(let i = 0; i < arr.length; i++) {
 
+  }
+}
 
 
 ///////////////////Problem 4///////////////////
@@ -78,7 +84,13 @@ var sampleOutput = [
 //getVals should return the new array.
 
 //CODE HERE
-
+function getVals(obj) {
+  let emptyArray = [];
+  for(let key in obj){
+    emptyArray.push(obj[key])
+  };
+  return emptyArray;
+}
 
 ///////////////////Problem 5///////////////////
 
@@ -93,3 +105,4 @@ var doors = {
 }
 
 //Code here
+let doorKeys = Object.keys(doors);
